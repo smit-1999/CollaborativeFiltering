6c88d10cb0a7ae93c87682452f6dc0afbf3c505a
Smit Shah <smitshah1999@gmail.com>
2019-11-03 19:41:07 +0530

    adds method to fetch dataset

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hi")
+import os
+import pandas as pd
+
+
+def get_dataset_directory():
+    curr_dir = os.getcwd()  # gets the current working directory
+    dataset_dir = os.path.join(curr_dir, "dataset")  # concatenates
+    return dataset_dir
+
+
+def initialise():
+    dataset_directory = get_dataset_directory()
+
+
+def main():
+    utility_matrix = initialise()
+
+
+main()
+
